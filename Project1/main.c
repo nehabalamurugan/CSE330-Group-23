c7b8f21f0c11fc14d1a4aa70d4ed7e2736eaadaf
Punit Arani <punitsai36@gmail.com>
2023-01-13 08:48:48 -0700

    Add module functions

--- Project1/main.c
+++ Project1/main.c
@@ -2,3 +2,28 @@
 // Group 23
 //
 // main file
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+
+/**
+ * Module function to run at load
+*/
+int main_init(void) {
+    printk("Hello World!\n");
+    return 0;
+}
+
+/**
+ * Module function to run at unload
+*/
+void main_exit(void) {
+    printk("Goodbye World!\n");
+}
+
+
+// Bind the functions
+module_init(main_init);
+module_exit(main_exit);
+MODULE_LICENSE("GPL");
