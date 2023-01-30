8cf09d4f11e7dd98b7210baaeac1e77256e6f51f
Punit Arani <punitsai36@gmail.com>
2023-01-30 12:17:32 -0700

    Update imports

--- Project1/Step5/syscall_test.c
+++ Project1/Step5/syscall_test.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
 #include <linux/kernel.h>
 #include <sys/syscall.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+
 
 int main() {
     syscall(450);
