22d316598c49c7839408b1fd8cf42a69cebe0934
Punit Arani <punitsai36@gmail.com>
2023-02-21 15:50:21 -0700

    Add producer()

--- Project2/producer_consumer.c
+++ Project2/producer_consumer.c
@@ -4,7 +4,10 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 #include <linux/semaphore.h>
+#include <linux/types.h>
 
 
 #define AUTHORS     "Punit Sai Arani, Neha Balamurugan, Siddhesh Nair, Arnav Sangelkar"
@@ -33,6 +36,82 @@ module_param(uuid, int, 0644);      // The UID of the user
 struct semaphore empty;
 struct semaphore full;
 struct semaphore mutex;
+
+// Counters
+size_t task_count = 0;
+
+
+// Task structs
+typedef struct task_struct TaskStruct;
+TaskStruct *producers;
+TaskStruct *consumers;
+
+
+// Node struct
+typedef struct node {
+    int pid;
+    int uid;
+    int item_num;
+    long time;
+    long start_time;
+} Node;
+
+
+// Buffer linked list
+typedef struct buffer {
+    Node node;
+    struct buffer *next;
+} Buffer;
+
+// Buffer stack
+Buffer *buffer;
+
+
+static int producer(void *arg) {
+    Node node;
+    TaskStruct *task;
+
+    // Iterate through the task list
+    for_each_process(task) {
+
+        // Check if the task uid matches
+        if (task->cred->uid.val == uuid) {
+            // Semaphore down
+            down_interruptible(&empty);
+            down_interruptible(&mutex);
+
+            task_count ++;
+
+            // Fill node
+            node = (Node) {
+                .pid = task->pid,
+                .uid = task->cred->uid.val,
+                .item_num = task_count,
+                .time = 0,
+                .start_time = task->start_time
+            };
+
+            // Add node to buffer
+            if (buffer == NULL) {
+                buffer->node = node;
+                buffer->next = NULL;
+            } else {
+                Buffer *next = buffer;
+                buffer->node = node;
+                buffer->next = next;
+            }
+
+            // Print process info
+            printk(KERN_INFO "Producer-1 Produced Item#-%d at buffer index: %zu for PID:%d", node.item_num, task_count, node.pid);
+
+            // Semaphore up
+            up(&mutex);
+            up(&full);
+        }
+    }
+
+    return 0;
+}
 
 
 int init_module(void) {
