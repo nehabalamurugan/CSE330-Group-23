0b6028adb65ebc440ae92d5d1492152c4cb54626
Punit Arani <punitsai36@gmail.com>
2023-02-21 08:12:16 -0700

    Add module params

--- Project2/producer_consumer.c
+++ Project2/producer_consumer.c
@@ -2,10 +2,24 @@
 // Group 23
 
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/kernel.h>
 
 
 MODULE_LICENSE("GPL");
+
+
+// Default params
+static int buffSize = 1024;
+static int prod = 1;
+static int cons = 1;
+static uid_t uuid = -1;
+
+// Define params
+module_param(buffSize, int, 0644);  // The buffer size
+module_param(prod, int, 0644);      // number of producers(0 or 1)
+module_param(cons, int, 0644);      // number of consumers(a non-negative number)
+module_param(uuid, int, 0644);      // The UID of the user
 
 
 int init_module(void) {
